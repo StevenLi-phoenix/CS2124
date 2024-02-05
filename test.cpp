ccae58dec35ac14da2c815540dc359b45476d14c
lishuyu <lishuyustevenli@163.com>
2024-02-05 16:48:10 -0500

    auto commit

--- test.cpp
+++ test.cpp
@@ -13,27 +13,33 @@ public:
             this->month = month;
             this->year = year;
         };
-        friend ostream& operator<<(ostream& os, const Date& date);
+        friend ostream& operator<<(ostream& os, const Date& date){
+            os << date.day << "/" << date.month << "/" << date.year;
+            return os;
+        };
     private:
         int day, month, year;
     };
 
     Person(const string& name, int day, int month, int year): name(name), date_of_brith(day, month, year){};
-    friend ostream& operator<<(ostream& os, const Person& person);
+    friend ostream& operator<<(ostream& os, const Person& person) {
+        os << person.name << " " << person.date_of_brith;
+        return os;
+    }
 private:
     string name;
     Date date_of_brith;
 };
 
-ostream& operator<<(ostream& os, const Person::Date& date){
-    os << date.day << "/" << date.month << "/" << date.year;
-    return os;
-}
+// ostream& operator<<(ostream& os, const Person::Date& date){
+//     os << date.day << "/" << date.month << "/" << date.year;
+//     return os;
+// }
 
-ostream& operator<<(ostream& os, const Person& person){
-    os << person.name << " " << person.date_of_brith;
-    return os;
-}
+// ostream& operator<<(ostream& os, const Person& person){
+//     os << person.name << " " << person.date_of_brith;
+//     return os;
+// }
 
 int main(){
     Person person = Person("Ahmed", 1, 1, 2020);
