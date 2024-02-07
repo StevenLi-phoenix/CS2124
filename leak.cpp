4072290b8fe9c30b8e81bf3aebe31cae455fe9b0
lishuyu <lishuyustevenli@163.com>
2024-02-07 16:40:35 -0500

    auto commit

--- leak.cpp
+++ leak.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a = 0;
     int* p = &a;
-    for (int i = 0; i < 1024; i++) {
+    for (int i = 0; i < 64; i++) {
         p++;
         cout << *p << endl;
     }
