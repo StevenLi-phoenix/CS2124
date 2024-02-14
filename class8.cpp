3b785709d161ea96a855327027361a8d254edbae
lishuyu <lishuyustevenli@163.com>
2024-02-14 15:43:40 -0500

    auto commit

--- class8.cpp
+++ class8.cpp
@@ -7,18 +7,46 @@ using namespace std;
 // global variables
 
 // class definitions
+class Person{
+    public:
+        string name;
+        int age;
+        Person(string name, int age){
+            this->name = name;
+            this->age = age;
+        }
+        Person(string name, int age = -1){
+            this->name = name;
+            this->age = age;
+        }
+        Person(int age, string name = "Unknown"){
+            this->name = name;
+            this->age = age;
+        }
+        Person(){
+            this->name = "Unknown";
+            this->age = -1;
+        }
+};
 
 // function prototypes
-int getNum(int num1, int num2);
+
 
 // main function
 int main(){
-    cout << getNum(5) << endl;
-    cout << getNum(5, 6) << endl;
+    vector<Person> people;
+    people.push_back(Person("Mike", 40));
+    people.push_back(Person(30));
+    people.push_back(Person("Vicky"));
+    people.push_back(Person());
+
+    for (const Person& p: people){
+        cout << p.name << " " << p.age << endl;
+    }
     return 0;
 }
 
 // function definitions
-int getNum(int num1, int num2 = 0){
+int getNum(int num1, int num2){
     return num1 + num2;
 }
