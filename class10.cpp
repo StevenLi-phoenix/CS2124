c07626e7b76adb42dc50d005197bbc7b2c676840
lishuyu <lishuyustevenli@163.com>
2024-03-04 15:42:59 -0500

    auto commit

--- class10.cpp
+++ class10.cpp
@@ -1,9 +1,9 @@
 /*
 Identity_id: 75ef2cd7-67b2-479c-8aca-988b25527998
 Author: Steven Li
-Time: 2024/02/26
-TIME: 1708979709
-Briefly: Class 10 class note
+Time: 2024/02/28
+TIME: 1709152755
+Briefly: Class 10 class note & Class 11 class note
 Purpose: note
 Usage: ./r class10.cpp
 Licence: CC BY-NC-SA 4.0
@@ -19,6 +19,14 @@ using namespace std;
 
 // class definitions
 class Vector{
+    /**
+     * Overloaded insertion operator for Vector class.
+     * Prints the capacity, size, and data elements of the Vector object to the output stream.
+     *
+     * @param os The output stream to write to.
+     * @param v The Vector object to be printed.
+     * @return The output stream after printing the Vector object.
+     */
     friend ostream& operator<<(ostream& os, const Vector& v){
         os << "capacity: " << v.capacity << " size: " << v.sizeValue << " data: ";
         for (size_t ii = 0; ii < v.sizeValue; ii++) {
@@ -27,15 +35,42 @@ class Vector{
         return os;
     };
     public:
+        /**
+         * Default constructor for the Vector class.
+         * Initializes the capacity, size, and data pointer.
+         */
         Vector() : capacity(0), sizeValue(0), data(nullptr) {}
+        /**
+         * @brief Constructs a Vector object with a specified size and initial value.
+         * 
+         * @param s The size of the Vector.
+         * @param val The initial value for each element in the Vector. Default value is 0.
+         */
         Vector(size_t s, int val = 0) : capacity(s), sizeValue(s) {
             data = new int[s];
             for (size_t ii = 0; ii < s; ii++) {
                 data[ii] = val;
             }
         }
+        /**
+         * @brief Destructor for the Vector class.
+         * 
+         * This destructor is responsible for deallocating the memory
+         * occupied by the dynamic array 'data'.
+         */
         ~Vector() { delete[] data; }
+        /**
+         * Returns the size of the object.
+         *
+         * @return The size of the object.
+         */
         size_t size() const { return sizeValue; }
+        /**
+         * Adds an element to the end of the container.
+         *
+         * @param val The value to be added.
+         * @return True if the value was successfully added, false otherwise.
+         */
         bool push_back(int val) {
             if (sizeValue == capacity) {
                 if (capacity == 0) {
@@ -49,9 +84,17 @@ class Vector{
             sizeValue++;
             return true;
         }
+        /**
+         * Clears the contents of the object.
+         */
         void clear() {
             sizeValue = 0;
         }
+        /**
+         * Removes and returns the last element of the container.
+         *
+         * @return The value of the last element that was removed.
+         */
         int pop_back() {
             if (sizeValue == 0) {
                 cerr << "pop_back: empty vector" << endl;
@@ -66,6 +109,14 @@ class Vector{
             }
             return data[sizeValue];
         }
+        /**
+         * @brief Overloaded subscript operator.
+         * 
+         * This function allows accessing elements of the object using the subscript operator [].
+         * 
+         * @param index The index of the element to access.
+         * @return int& A reference to the element at the specified index.
+         */
         int operator[](size_t index) {
             // return *(data+index);
             if (index >= sizeValue) return -1;
@@ -74,6 +125,15 @@ class Vector{
             return -1;
         }
     private:
+        /**
+         * Resizes the array to the specified new size.
+         * If the new size is greater than the current capacity, the array is reallocated with the new size.
+         * If the new size is smaller than the current capacity, the array is reallocated only if the new size is greater than the current size.
+         * If the new size is smaller than the current size, the array cannot be shrunk and the function returns false.
+         * 
+         * @param newsize The new size of the array.
+         * @return true if the array was successfully resized, false otherwise.
+         */
         bool resize(size_t newsize) {
             if (newsize > capacity) {
                 int* temp = new int[newsize];
---
+++ class11.cpp
@@ -0,0 +1,56 @@
+/*
+Identity_id: bbc475ca-bb1f-449e-8ac1-1bbd6f60d2bf
+Author: Steven Li
+Time: 2024/03/04
+TIME: 1709584361
+Briefly: 
+Purpose: 
+Usage:
+Licence: CC BY-NC-SA 4.0
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+// global variables
+
+// class definitions
+class Vector{
+    friend ostream& operator<<(ostream& os, const Vector& v){
+        os << "Vector(" << v.size() << ")\n";
+        for(int i = 0; i < v.size(); i++){
+            os << v.elem[i] << " ";
+        }
+        return os;
+    };
+
+    public:
+        Vector(int s) : elem(new double[s]), sz(s) {}
+        Vector& operator=(const Vector& a);
+        // Vector& operator=(int i){
+        //     for(int j = 0; j < sz; j++){
+        //         elem[j] = i;
+        //     }
+        //     return *this;
+        // };
+        double& operator[](int i) { return elem[i]; }
+        int size() const { return sz; }
+    private:
+        double* elem;
+        int sz;
+};
+// function prototypes
+
+// main function
+int main() {
+    Vector v1(5);
+    v1 = 3;
+    cout << v1 << endl;
+    return 0;
+}
+
+// function definitions
+
